459bf2ecf7b0b4d985da7a647c16e9f07dedf802
ivominic <ivo.minic@gmail.com>
2024-02-09 19:55:39 +0100

    Pdf parsing for Addiko bank completed

--- src/banks/addiko.service.ts
+++ src/banks/addiko.service.ts
@@ -12,27 +12,15 @@ export class AddikoPdfService {
     }
     const clientData = this.readClientData(data.pages[0].content);
     retVal = { ...retVal, ...clientData };
-    console.log('retVal', retVal);
-    /*
-    
-    const summaryTable = this.readSummaryTable(data.pages[0].content);
-    retVal = { ...retVal, ...clientData, ...summaryTable };
 
     let tableArray = [];
-    let isValidTable = true;
     for (let i = 0; i < data.pages.length; i++) {
-      if (isValidTable) {
-        const tempArray = this.readMainTable(data.pages[i].content, i);
-        tableArray = [...tableArray, ...tempArray];
-      }
-      data.pages[i].content.forEach((element) => {
-        if (element.str === 'NEIZVRŠENI NALOZI') {
-          isValidTable = false;
-        }
-      });
+      const tempArray = this.readMainTable(data.pages[i].content, tableArray);
+      const tempAdditionalData = this.readSummaryTable(data.pages[i].content);
+      retVal = { ...retVal, ...tempAdditionalData };
+      tableArray = [...tableArray, ...tempArray];
     }
-    retVal['table'] = tableArray;*/
-    retVal = JSON.stringify(data.pages[0].content);
+    retVal['table'] = tableArray;
 
     return retVal;
   }
@@ -89,6 +77,251 @@ export class AddikoPdfService {
       }
     });
 
+    return retVal;
+  }
+
+  readMainTable(content, existingArray) {
+    const sequenceText: string = 'Broj',
+      additionalSequenceText: string = 'Nal',
+      receiverText: string = 'Naziv i sjedište',
+      originText: string = 'Porijeklo naloga',
+      debtText: string = 'Duguje',
+      feeText: string = 'Potražuje',
+      codeText: string = 'Osnov',
+      purposeText: string = 'Svrha',
+      modelText: string = 'Poziv na broj',
+      complaintText: string = 'Obračunata naknada',
+      workerText: string = 'Radnik';
+    let commonY = 250,
+      sequenceX = 49,
+      receiverX,
+      originX,
+      debtX,
+      feeX,
+      codeX,
+      purposeX,
+      modelX,
+      complaintX,
+      workerX;
+    const margin = 5;
+    const tempArray = [],
+      yArray = [];
+    const maxY = this.findMaxY(content);
+
+    content.forEach((element) => {
+      const value = element.str.trim();
+      if (value === additionalSequenceText) {
+        //sequenceX = element.x;
+        commonY = element.y;
+      }
+      value === receiverText && (receiverX = element.x);
+      value === originText && (originX = element.x);
+      value === debtText && (debtX = element.x);
+      value === feeText && (feeX = element.x);
+      value === codeText && (codeX = element.x);
+      value === purposeText && (purposeX = element.x);
+      value === modelText && (modelX = element.x);
+      value === complaintText && (complaintX = element.x);
+      value === workerText && (workerX = element.x);
+
+      if (value && element.y > commonY && element.x <= sequenceX) {
+        if (value !== sequenceText && value !== additionalSequenceText) {
+          yArray.push(element.y);
+        }
+      }
+    });
+
+    //When page break goes through a row. We need to append to the last item of previous page
+    if (existingArray.length) {
+      content.forEach((element) => {
+        const value = element.str.trim();
+        if (
+          ![
+            'Naziv i sjedište',
+            'primaoca nalogodavca',
+            'Broj računa',
+            'Porijeklo naloga',
+            'Datum knjiženja',
+            'Poziv na broj',
+            'zaduženja/odobrenja',
+          ].includes(value)
+        )
+          if (element.y < yArray[0] && element.x === 61) {
+            const rgx = /^[0-9,\-]*$/;
+            if (rgx.test(value)) {
+              existingArray[existingArray.length - 1]['partnerAccount'] = value;
+            } else {
+              existingArray[existingArray.length - 1]['receiver'] +=
+                ' ' + value;
+            }
+            if (element.x >= 144 && element.x < 200) {
+              const dateArray = value.split('.');
+              if (
+                dateArray.length === 3 &&
+                dateArray[0].length === 4 &&
+                dateArray[1].length === 2 &&
+                dateArray[2].length === 2
+              ) {
+                existingArray[existingArray.length - 1]['dateOrigin'] = value;
+              }
+            }
+            if (element.x >= 555 && element.x < 560) {
+              existingArray[existingArray.length - 1]['modelApprove'] = value;
+            }
+          }
+      });
+    }
+
+    for (let i = 0; i < yArray.length; i++) {
+      const y = yArray[i];
+      let nextY = y + 100;
+      maxY > 0 && (nextY = maxY);
+      i < yArray.length - 1 && (nextY = yArray[i + 1]);
+      const tempVal = {};
+      tempVal['modelDebt'] = '';
+      tempVal['modelApprove'] = '';
+      tempVal['calculatedExpense'] = '';
+      tempVal['worker'] = '';
+
+      content.forEach((element) => {
+        const value = element.str.trim();
+
+        if (value && element.y > y - margin && element.y < nextY - margin) {
+          const x = element.x;
+          if (x > 48 && x < 61) {
+            if (value.indexOf('. ') && value.indexOf('. ') < 3) {
+              tempVal['sequenceNumber'] = value.substring(
+                0,
+                value.indexOf('. '),
+              );
+              tempVal['receiver'] = value.substring(value.indexOf('. ') + 2);
+            }
+          } else if (x === 61) {
+            const rgx = /^[0-9,\-]*$/;
+            if (rgx.test(value)) {
+              tempVal['partnerAccount'] = value;
+            } else {
+              if (tempVal['receiver']) {
+                tempVal['receiver'] += ' ' + value;
+              } else {
+                tempVal['receiver'] = value;
+              }
+            }
+          }
+          if (x >= 144 && x < 200) {
+            const dateArray = value.split('.');
+            if (
+              dateArray.length === 3 &&
+              dateArray[0].length === 2 &&
+              dateArray[1].length === 2 &&
+              dateArray[2].length === 4
+            ) {
+              tempVal['dateOrigin'] = value;
+            } else {
+              if (tempVal['origin']) {
+                tempVal['origin'] += ' ' + value;
+              } else {
+                tempVal['origin'] = value;
+              }
+            }
+          }
+          if (x >= 310 && x < 330) {
+            tempVal['debt'] = value;
+          }
+          if (x >= 390 && x < 400) {
+            tempVal['fee'] = value;
+          }
+          if (x >= 427 && x < 430) {
+            tempVal['code'] = value;
+          }
+          if (x >= 452 && x < 460) {
+            if (tempVal['purpose']) {
+              tempVal['purpose'] += ' ' + value;
+            } else {
+              tempVal['purpose'] = value;
+            }
+          }
+          if (x >= 555 && x < 560) {
+            if (element.y >= y && element.y < y + 10) {
+              tempVal['modelDebt'] = value;
+            } else {
+              tempVal['modelApprove'] = value;
+            }
+          }
+          if (x >= 650 && x < 660) {
+            tempVal['calculatedExpense'] = value;
+          }
+          if (x >= 749) {
+            tempVal['worker'] = value;
+          }
+        }
+      });
+      tempArray.push(tempVal);
+    }
+
+    return tempArray;
+  }
+
+  readSummaryTable(content) {
+    const initialStateText: string = 'Prethodno stanje',
+      owesText: string = 'Duguje',
+      demandsText: string = 'Potražuje',
+      newStateText: string = 'Novo stanje',
+      numberAccountsText: string = 'Br.Nal.Potražuje Br.Nal.Duguje';
+    let commonY, initialStateX, owesX, demandsX, newStateX, numberAccountsX;
+    const margin = 30;
+    const retVal = {};
+
+    content.forEach((element) => {
+      const value = element.str.trim();
+      if (value === initialStateText) {
+        initialStateX = element.x;
+        commonY = element.y;
+      }
+      value === owesText && (owesX = element.x);
+      value === demandsText && (demandsX = element.x);
+      value === newStateText && (newStateX = element.x);
+      value === numberAccountsText && (numberAccountsX = element.x);
+    });
+
+    if (initialStateX && numberAccountsX) {
+      content.forEach((element) => {
+        const value = element.str.trim();
+        if (value && element.y > commonY && element.y < commonY + 20) {
+          const x = element.x;
+          if (x >= initialStateX && x < owesX - margin) {
+            retVal['initialState'] = value;
+          }
+          if (x >= owesX - margin && x < demandsX - margin) {
+            retVal['owes'] = value;
+          }
+          if (x >= demandsX - margin && x < newStateX - margin) {
+            retVal['demands'] = value;
+          }
+          if (x >= newStateX - margin && x < numberAccountsX - margin) {
+            retVal['newState'] = value;
+          }
+          if (x >= numberAccountsX) {
+            if (x < 748) {
+              retVal['numberAccountsDemands'] = value;
+            } else {
+              retVal['numberAccountsDebts'] = value;
+            }
+          }
+        }
+      });
+    }
+
+    return retVal;
+  }
+
+  findMaxY(content) {
+    let retVal = 0;
+    const searchValue = 'Br.Nal.Potražuje Br.Nal.Duguje';
+    content.forEach((element) => {
+      const value = element.str.trim();
+      value === searchValue && (retVal = element.y);
+    });
     return retVal;
   }
 }
